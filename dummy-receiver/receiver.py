66f88c1d48910b63fb0220f18db137033ffee4b7
Mateusz Kalinowski <mateusz.kalinowski@interia.eu>
2020-06-28 22:45:50 +0200

    Project: Basic docker compose setup for the application

--- dummy-receiver/receiver.py
+++ dummy-receiver/receiver.py
@@ -1,21 +1,32 @@
 #!/usr/bin/env python
 import pika
-
-credentials = pika.PlainCredentials('user', 'user')
-
-connection = pika.BlockingConnection(
-    pika.ConnectionParameters(host='rabbitmq-broker', credentials=credentials))
-channel = connection.channel()
-
-channel.queue_declare(queue='hello')
-
+from retry import retry
 
 def callback(ch, method, properties, body):
     print(" [x] Received %r" % body)
 
+credentials = pika.PlainCredentials('user', 'user')
 
-channel.basic_consume(
-    queue='hello', on_message_callback=callback, auto_ack=True)
+print("receiver: started ")
 
-#print(' [*] Waiting for messages. To exit press CTRL+C')
-channel.start_consuming()
+@retry(pika.exceptions.AMQPConnectionError, delay=5, jitter=(1, 3))
+def consume():
+    print("receiver: attempting to connect")
+
+    connection = pika.BlockingConnection(
+        pika.ConnectionParameters(host='rabbitmq-broker', credentials=credentials))
+
+    channel = connection.channel()
+    channel.queue_declare(queue='hello')
+    channel.basic_consume(
+        queue='hello', on_message_callback=callback, auto_ack=True)
+
+    #print(' [*] Waiting for messages. To exit press CTRL+C')
+    try:
+        channel.start_consuming()
+    except KeyboardInterrupt:
+        channel.stop_consuming()
+        connection.close()
+
+consume()
+
--- sensors-middleware/sender.py
+++ sensors-middleware/sender.py
@@ -1,14 +1,21 @@
 #!/usr/bin/env python
 import pika
+from retry import retry
 
 credentials = pika.PlainCredentials('user', 'user')
 
-connection = pika.BlockingConnection(
-    pika.ConnectionParameters(host='rabbitmq-broker', credentials=credentials))
-channel = connection.channel()
+@retry(pika.exceptions.AMQPConnectionError, delay=5, jitter=(1, 3))
+def consume():
+    print("sender: attempting to connect")
+    
+    connection = pika.BlockingConnection(
+        pika.ConnectionParameters(host='rabbitmq-broker', credentials=credentials))
 
-channel.queue_declare(queue='hello')
+    channel = connection.channel()
+    channel.queue_declare(queue='hello')
+    channel.basic_publish(exchange='', routing_key='hello', body='Hello World!')
+    
+    print(" [x] Sent 'Hello World!'")
+    connection.close()
 
-channel.basic_publish(exchange='', routing_key='hello', body='Hello World!')
-print(" [x] Sent 'Hello World!'")
-connection.close()
+consume()
